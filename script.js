a6367d96759df484bda409d14861de1cc8b43d07
Riccardo Martelli <35230820+Riccardo-Martelli@users.noreply.github.com>
2024-05-03 21:33:53 +0200

    Add files via upload
    bug fixed

--- script.js
+++ script.js
@@ -15,19 +15,6 @@ const observer = new IntersectionObserver((entries) => {
 sectionElem.forEach((el) => observer.observe(el));
 
 // Add coping on hover
-//https://medium.com/@tamilnambicse/copy-text-with-the-click-of-a-button-using-clipboard-api-in-javascript-a7f53abdc310
-/*document.getElementById("copyButton").addEventListener("click", function () {
-  var copyText = document.getElementById("copyText").value;
-
-  navigator.clipboard
-    .writeText(copyText)
-    .then(function () {
-      alert("Text copied to clipboard: " + copyText);
-    })
-    .catch(function (error) {
-      alert("Failed to copy text: " + error);
-    });
-});*/
 
 function protoHover1() {
     navigator.clipboard.writeText("riccardomartelli97@gmail.com");
