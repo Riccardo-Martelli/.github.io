e4f9dee67e8abd2fae75294bd0787f002a0ad4f7
Riccardo Martelli <35230820+Riccardo-Martelli@users.noreply.github.com>
2024-05-28 17:18:04 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -18,11 +18,8 @@ window.addEventListener('scroll',() => {
   let value = window.scrollY;
    /*Welcome.style.left= value * 2.5 +'px';*/
    /*Welcome.style.marginRight= value * -2.5 +'px';*/
-   /*Welcome.style.opacity = Math.abs(1 - 8.5*value/document.documentElement.scrollHeight);
-   if((1 - 8.5*value/document.documentElement.scrollHeight) ===0){
-    Welcome.style.display="none";
-   }
-   */
+   Welcome.style.opacity = Math.abs(1 - 8.5*value/document.documentElement.scrollHeight);
+ 
     
 
   /* whiteDwarf.style.left = value * -5.5 +'px';*/
