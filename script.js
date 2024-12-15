8c9033da208328e740729ec78f56d218752046d7
Riccardo Martelli <35230820+Riccardo-Martelli@users.noreply.github.com>
2024-12-15 11:55:14 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -377,7 +377,7 @@ document.addEventListener('click', function(event) {
           case "Geometria Differenziale":case "Metodi Matematici per la Meccanica Classica":case "Gruppi di Lie e Algebre di Lie":case "Analisi Funzionale":case "Serie di Fourier":
           case "Fourier Series":case "Mathematical Method for Classical Mechanics":case "Relatività Generale":case "Teoria dei Grouppi per Modelli matematici":
           case "Differential Geometry": case "Lie Groups and Lie algebras": case "Functional Analysis":
-          case "Fluid Dynamics": case"Fluidodinamica": case"General Relativity": case "Group Theory for Mathematical Modeling":
+          case "R": case "MATLAB": case "Fluid Dynamics": case"Fluidodinamica": case"General Relativity": case "Group Theory for Mathematical Modeling":
               return '25€';
           case "Linear Algebra":case "Algebra Lineare":case"Termodinamica":case"Meccanica Classica":
           case "C++":case "C":case "Python":case "HTML":case "CSS":case "JavaScript":case "Mathematica":case "Tensorflow":case "Keras":case "Scikit-learn":case "SQL":case "Bash":case "Cuda":case "Sed":case "Awk":case "Data Analysis":
